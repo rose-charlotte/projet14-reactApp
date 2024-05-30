e16b3abd24bfe8e2a98a5603023a814cd7021c82
rose-charlotte <charlotte@anros.fr>
2024-05-30 09:32:34 +0200

    HJome modification

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
+import { InputElement } from "../../components/Commons/Checkbox/InputElement";
 
 export function Home() {
     return (
-        <div>
-            <h1>HRnet</h1>
-            <Link to="/">View current Employees</Link>
-            <h2>Create Employee</h2>
-        </div>
+        <>
+            <div>
+                <h1>HRnet</h1>
+                <Link to="/">View current Employees</Link>
+                <h2>Create Employee</h2>
+            </div>
+            <div>
+                <InputElement label="First Name" name="firstName" />
+                <InputElement label="Last Name" name="lastName" />
+            </div>
+        </>
     );
 }

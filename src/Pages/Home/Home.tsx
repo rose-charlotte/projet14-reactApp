9d4abf9b75d9831471de3d4f3ce5cf45caced57d
rose-charlotte <charlotte@anros.fr>
2024-05-30 09:33:35 +0200

    name changes

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { InputElement } from "../../components/Commons/Checkbox/InputElement";
+import { InputElement } from "../../components/Commons/InputElement/InputElement";
 
 export function Home() {
     return (

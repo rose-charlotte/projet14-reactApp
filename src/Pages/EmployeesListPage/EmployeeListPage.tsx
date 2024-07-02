9b8571730fee65e2153796e3d17148fbe3175b06
rose-charlotte <charlotte@anros.fr>
2024-07-02 13:38:09 +0200

    setUp input search+ + select

--- src/Pages/EmployeesListPage/EmployeeListPage.tsx
+++ src/Pages/EmployeesListPage/EmployeeListPage.tsx
@@ -2,126 +2,24 @@ import { Link } from "react-router-dom";
 import style from "./EmployeeListPage.module.scss";
 
 import { TableContainer } from "../../components/Table/TableContainer/TableContainer";
-//import { Employee } from "../../data/models/Employee.ts";
-//import { getEmployees } from "../../data/employeeRepository.ts";
-//import { useEffect, useState } from "react";
+import { SearchElement } from "../../components/SearchElement/SearchElement";
+import { SelectElement } from "../../components/Commons/SelectElement/SelectElement";
 
 export function EmployeeListPage() {
-    // const tableElements = [
-    //     "First Name",
-    //     "Last Name",
-    //     "Start Date",
-    //     "Department",
-    //     "Date of Birth",
-    //     "Street",
-    //     "City",
-    //     "State",
-    //     "Zip Code",
-    // ];
-    // const [employees, setEmployees] = useState<Employee[]>();
-    // const [sorted, setSorted] = useState(false);
-
-    // useEffect(() => {
-    //     async function fetchEmployees() {
-    //         setEmployees(await getEmployees());
-    //     }
-
-    //     fetchEmployees();
-    // }, []);
-
-    // if (!employees) {
-    //     return <span>Loading...</span>;
-    // }
-
-    // const handleSort = (index: number, ele: string) => {
-    //     console.log(index, ele);
-    //     const type = ele.split(" ").join("");
-
-    //     const employeesCopy = [...employees];
-
-    //     switch (type) {
-    //         case "FirstName":
-    //             if (!sorted) {
-    //                 console.log("pas sorted");
-
-    //                 setEmployees(
-    //                     employeesCopy.sort((e1, e2) =>
-    //                         e1.firstName < e2.firstName ? -1 : e1.firstName > e2.firstName ? 1 : 0
-    //                     )
-    //                 );
-    //                 setSorted(true);
-    //             } else {
-    //                 console.log("je suis sorted");
-    //                 console.log(employees);
-
-    //                 setEmployees(
-    //                     employeesCopy.sort((e1, e2) =>
-    //                         e1.firstName < e2.firstName ? 1 : e1.firstName > e2.firstName ? -1 : 0
-    //                     )
-    //                 );
-    //                 setSorted(false);
-    //             }
-
-    //             break;
-
-    //         case "LastName":
-    //             if (!sorted) {
-    //                 console.log("pas sorted");
-
-    //                 setEmployees(
-    //                     employeesCopy.sort((e1, e2) =>
-    //                         e1.lastName < e2.lastName ? -1 : e1.lastName > e2.lastName ? 1 : 0
-    //                     )
-    //                 );
-    //                 setSorted(true);
-    //             } else {
-    //                 console.log("je suis sorted");
-    //                 console.log(employees);
-
-    //                 setEmployees(
-    //                     employeesCopy.sort((e1, e2) =>
-    //                         e1.lastName < e2.lastName ? 1 : e1.lastName > e2.lastName ? -1 : 0
-    //                     )
-    //                 );
-    //                 setSorted(false);
-    //             }
-    //             break;
-    //         default:
-    //             console.log("ca marche pas");
-    //     }
-    // };
-
     return (
         <div className={style.container}>
-            <TableContainer />
-            {/* <h1>Current Employee</h1>
-
-            <table>
-                <thead className={style.tableHeader}>
-                    <tr>
-                        {tableElements.map((ele, index) => (
-                            <th key={index}>
-                                {ele} <button onClick={() => handleSort(index, ele)}>v</button>
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {employees.map((employee, index) => (
-                        <tr className={style.employeeList} key={index}>
-                            <td>{employee.firstName}</td>
-                            <td>{employee.lastName}</td>
-                            <td>{employee.startDate?.toLocaleString().slice(0, 10)}</td>
-                            <td>{employee.department}</td>
-                            <td>{employee.dateOfBirth?.toLocaleString().slice(0, 10)}</td>
-                            <td>{employee.street}</td>
-                            <td>{employee.city}</td>
-                            <td>{employee.state}</td>
-                            <td>{employee.zipCode}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table> */}
+            <h1>Current Employees</h1>
+            <div className={style.elements}>
+                <nav className={style.nav}>
+                    <SelectElement
+                        label="number of entries"
+                        name="number of employees"
+                        arrayOfElement={["5", "10", "20", "50", "100"]}
+                    />
+                    <SearchElement />
+                </nav>
+                <TableContainer />
+            </div>
 
             <Link to="/">Home</Link>
         </div>

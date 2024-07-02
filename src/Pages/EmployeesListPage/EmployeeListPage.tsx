97d9bec13d3972d7584d31a128bf403e17a49e73
rose-charlotte <charlotte@anros.fr>
2024-07-02 14:06:15 +0200

    setUo indication du nombre d'employyees affiché

--- src/Pages/EmployeesListPage/EmployeeListPage.tsx
+++ src/Pages/EmployeesListPage/EmployeeListPage.tsx
@@ -4,8 +4,24 @@ import style from "./EmployeeListPage.module.scss";
 import { TableContainer } from "../../components/Table/TableContainer/TableContainer";
 import { SearchElement } from "../../components/SearchElement/SearchElement";
 import { SelectElement } from "../../components/Commons/SelectElement/SelectElement";
+import { useEffect, useState } from "react";
+import { Employee } from "../../data/models/Employee";
+import { getEmployees } from "../../data/employeeRepository";
 
 export function EmployeeListPage() {
+    const [employees, setEmployees] = useState<Employee[]>();
+    useEffect(() => {
+        async function fetchEmployees() {
+            setEmployees(await getEmployees());
+        }
+
+        fetchEmployees();
+    }, []);
+
+    if (!employees) {
+        return <span>Loading...</span>;
+    }
+
     return (
         <div className={style.container}>
             <h1>Current Employees</h1>
@@ -18,7 +34,8 @@ export function EmployeeListPage() {
                     />
                     <SearchElement />
                 </nav>
-                <TableContainer />
+                <TableContainer employees={employees} />
+                <div>Showing {employees.length} entries</div>
             </div>
 
             <Link to="/">Home</Link>
